a29611b0367aec5ba96dc8853b271a213f220984
derek <github@derekgillett.com>
2016-03-16 20:18:44 +1100

    fix markup / markdown buttons. add message when markup not set

--- resources/views/invoiceitem/form.blade.php
+++ resources/views/invoiceitem/form.blade.php
@@ -4,7 +4,7 @@
     {!! Form::hidden('invoice_id', $invoice_id, $options) !!}
 
     <div class="form-group">
-        {!! Form::label('category', 'Category:', ['class' => 'control-label']) !!}
+        {!! Form::label('category', trans('invoice_item.category').':', ['class' => 'control-label']) !!}
 
         <?php $options['id'] = 'category_list' ?>
         {{ Form::select('category_id', ['' => ''] + $invoice_item_categories->toArray(), null, $options) }}
@@ -12,7 +12,7 @@
     </div>
 
     <div class="form-group">
-        {{ Form::label('description_label', 'Description:', ['class' => 'control-label']) }}
+        {{ Form::label('description_label', trans('invoice_item.description').':', ['class' => 'control-label']) }}
 
         <?php $options['id'] = 'description_list' ?>
         {{ Form::select('description', ['' => ''] + $invoice_item_list->toArray(), null, $options) }}
@@ -41,13 +41,20 @@
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-success']) !!}
 
     @if (!array_key_exists("disabled",$options))
-    <a class="btn btn-primary" id='btnMarkup'>
-        <i class="fa fa-btn fa-arrow-up"></i></a>
-    <a class="btn btn-primary" id='btnMarkDown'>
-        <i class="fa fa-btn fa-arrow-down"></i></a>
+        @if (Setting::get('markup') == '')
+        <br /><br />
+        <div class="alert alert-warning" role="alert">
+            <a href='/settings'>{{ trans('invoice_item.warning-markup-blank') }}</a>
+        </div>
+        @else
+        <a class="btn btn-primary" id='btnMarkup'>
+            <i class="fa fa-btn fa-arrow-up"></i></a>
+        <a class="btn btn-primary" id='btnMarkDown'>
+            <i class="fa fa-btn fa-arrow-down"></i></a>
+        @endif
     @endif
 
-@section('footer1')
+
 <script type="text/javascript">
 $( document ).ready(function() {
     var markUp = 1+{{ Setting::get('markup', 10)/100 }};
@@ -75,4 +82,3 @@ $( document ).ready(function() {
     });
 });
 </script>
-@stop
