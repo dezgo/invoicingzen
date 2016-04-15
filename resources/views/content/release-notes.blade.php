ffa5ffc1d32b57c1aa9483a54281c50fadd18203
derek <github@derekgillett.com>
2016-04-15 11:11:11 +1000

    release notes

--- resources/views/content/release-notes.blade.php
+++ resources/views/content/release-notes.blade.php
@@ -4,6 +4,14 @@
 <div class="panel">
     <h1>Invoicing Zen Release Notes</h1>
 
+    <h3>v1.1.12</h3>
+    <h4>15 Apr 2016</h4>
+    <ul>
+        <li>
+            Add email testing. Update footer to show when in development (9e935e0)
+        </li>
+    </ul>
+
     <h3>v1.1.11</h3>
     <h4>13 Apr 2016</h4>
     <ul>
