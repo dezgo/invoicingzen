2ea8083c879b0720aeb374cd1fcde95698ed1ae5
derek <github@derekgillett.com>
2016-04-28 23:50:21 +1000

    stats

--- app/Http/Controllers/SuperAdminController.php
+++ app/Http/Controllers/SuperAdminController.php
@@ -7,11 +7,26 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Invoice;
+use App\InvoiceItem;
+use App\Company;
 
 class SuperAdminController extends Controller
 {
     public function phpinfo()
     {
         return view('content.phpinfo');
+    }
+
+    public function stats()
+    {
+        $stats = ['total_invoices' => Invoice::all()->count(),
+                  'total_line_items' => InvoiceItem::all()->count(),
+                  'total_companies' => Company::all()->count(),
+                  'total_users' => User::all()->count(),
+                 ];
+
+        $companies = Company::all()->sortBy('created_at');
+        return view('content.stats', compact('stats', 'companies'));
     }
 }
---
+++ resources/views/content/stats.blade.php
@@ -0,0 +1,27 @@
+@extends('web')
+
+@section('content')
+<div class="container">
+
+    <h1>Stats</h1>
+    <div class="panel panel-default">
+        <div class="panel-heading">Totals</div>
+        <div class="panel-body">
+        Total Companies: {{ $stats['total_companies'] }}<br />
+        Total Users: {{ $stats['total_users'] }}<br />
+        Total Invoices: {{ $stats['total_invoices'] }}<br />
+        Total Line Items: {{ $stats['total_line_items'] }}<br />
+        </div>
+    </div>
+
+    <div class="panel panel-default">
+        <div class="panel-heading">Companies</div>
+        <div class="panel-body">
+            @foreach ($companies as $company)
+                {{ $company->created_at }}: {{ $company->company_name }}<br />
+            @endforeach
+        </div>
+    </div>
+</div>
+
+@stop
