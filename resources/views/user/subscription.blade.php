7d17a1cb025f156bbbede5f67b12da96d881acb3
derek <github@derekgillett.com>
2016-04-30 14:54:59 +1000

    merge stash

--- resources/views/user/subscription.blade.php
+++ resources/views/user/subscription.blade.php
@@ -2,15 +2,8 @@
 
 @section('content')
 @include('user.sidebar')
-{!! Form::open(['method' => 'POST', 'url' => url('/user/subscribe')]) !!}
-  <script
-    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-    data-key="{{ env('STRIPE_KEY') }}"
-    data-amount="{{ 900 }}"
-    data-name="Invoicing Zen"
-    data-description="InvoicingZen Standard"
-    data-image="{{ url('/images/'.Auth::user()->company->logofilename) }}"
-    data-locale="auto">
-  </script>
-  {!! Form::close() !!}
+<div class="form-group">
+    {!! Form::label('subscription', 'Subscription:', ['class' => 'control-label']) !!}
+    {{ Form::text('subscription', null, $options) }}
+</div>
 @stop
